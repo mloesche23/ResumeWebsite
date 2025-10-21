c9c04aff9d4e26b3d7938b9edcde4f29c855ec87
mloesche23 <loeschermichael06@gmail.com>
2025-10-21 19:09:40 -0400

    experience and education button scrolls

--- src/main.js
+++ src/main.js
@@ -1,10 +1,29 @@
-const experience = document.getElementById('work-experience-full');
-const experienceButton = document.getElementById('experience-button');
 
-function experienceBack() {
-  experience.scrollIntoView({
-    behavior: 'smooth',
+
+function scrollToSection(selector) {
+  const headerGap = 80;
+  const section = document.querySelector(selector);
+  const sectionTop = section.getBoundingClientRect().top + window.scrollY;
+  const offsetTop = sectionTop - headerGap;
+
+  window.scrollTo({
+    top: offsetTop,
+    behavior: 'smooth'
   });
 }
 
-experienceButton.addEventListener('click', experienceBack);
+document.getElementById("experience-button").addEventListener("click", () => {
+  scrollToSection(".work-experience-full");
+});
+
+document.getElementById("education-button").addEventListener("click", () => {
+  scrollToSection(".education");
+});
+
+document.getElementById("skills-button").addEventListener("click", () => {
+  scrollToSection(".skills");
+});
+
+document.getElementById("contact-button").addEventListener("click", () => {
+  scrollToSection(".contact");
+});
